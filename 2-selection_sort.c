af57ca5ea8e69594df1f2356b82b82d065434a7c
sezfabian <cheruiyotfabian@gmail.com>
2023-02-20 22:31:35 +0300

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,6 +13,9 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 	int temp, index;
 
+	if (size < 2 || array == NULL)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		temp = array[i];
